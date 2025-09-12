eaee72a85ac809a60a648c61505e166500ed5fb9
Alexander Aravin <sander-raven@yandex.ru>
2025-09-12 21:13:47 +0300

    feat: add DRF stuff for Run
    - ModelSerializer
    - ModelViewSet
    - DefaultRouter

--- app_run/views.py
+++ app_run/views.py
@@ -1,9 +1,17 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
 
 from .helpers import get_company_details
+from .models import Run
+from .serializers import RunSerializer
 
 
 @api_view(['GET'])
 def company_details(request):
     return Response(get_company_details())
+
+
+class RunViewSet(ModelViewSet):
+    queryset = Run.objects.all()
+    serializer_class = RunSerializer

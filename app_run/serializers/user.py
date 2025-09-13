e67b47d87304cf60c43be7b82d2289b2341f4059
Alexander Aravin <sander-raven@yandex.ru>
2025-09-13 20:02:05 +0300

    feat: add runs_finished field to UserSerializer

--- app_run/serializers/user.py
+++ app_run/serializers/user.py
@@ -2,12 +2,14 @@ from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
 from ..helpers import UserTypes
+from ..models import Run
 
 User = get_user_model()
 
 
 class UserSerializer(serializers.ModelSerializer):
     type = serializers.SerializerMethodField()
+    runs_finished = serializers.SerializerMethodField()
 
     class Meta:
         model = User
@@ -18,6 +20,7 @@ class UserSerializer(serializers.ModelSerializer):
             'last_name',
             'first_name',
             'type',
+            'runs_finished',
         )
 
     def get_type(self, obj):
@@ -25,3 +28,6 @@ class UserSerializer(serializers.ModelSerializer):
             return UserTypes.COACH.value
         else:
             return UserTypes.ATHLETE.value
+
+    def get_runs_finished(self, obj):
+        return obj.runs.filter(status=Run.Status.FINISHED).count()

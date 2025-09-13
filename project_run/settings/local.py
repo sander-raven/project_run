8107be55a1e052c9c29277b26d292848b3dd7ffd
Alexander Aravin <sander-raven@yandex.ru>
2025-09-13 12:22:47 +0300

    chore: add logging settings

--- project_run/settings/local.py
+++ project_run/settings/local.py
@@ -9,3 +9,22 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# Logging
+
+# LOGGING = {
+#     "version": 1,
+#     "disable_existing_loggers": False,
+#     "handlers": {
+#         "console": {
+#             "level": "DEBUG",
+#             "class": "logging.StreamHandler",
+#         },
+#     },
+#     "loggers": {
+#         "django.db.backends": {
+#             "handlers": ["console"],
+#             "level": "DEBUG",
+#         },
+#     },
+# }

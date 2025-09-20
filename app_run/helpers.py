603f13ab958dd8943748dbc10a13f56a52221178
Alexander Aravin <sander-raven@yandex.ru>
2025-09-20 18:12:32 +0300

    feat: add helper function `assign_nearby_items_to_user`

--- app_run/helpers.py
+++ app_run/helpers.py
@@ -1,7 +1,12 @@
 """Additional helper functions & classes"""
+import math
 from enum import Enum
+
 from django.conf import settings
 from django.db.models import Q
+from geopy.distance import geodesic
+
+from app_run.models import CollectibleItem, Position
 
 
 class UserTypes(Enum):
@@ -26,3 +31,36 @@ def get_company_details() -> dict:
         'slogan': getattr(settings, 'SLOGAN', ''),
         'contacts': getattr(settings, 'CONTACTS', ''),
     }
+
+
+def assign_nearby_items_to_user(
+        position: Position,
+        radius: float = 100,
+):
+    """Assign nearby collectible items to user"""
+    radius *= 1.1
+    user = position.run.athlete
+    user_latitude = position.latitude
+    user_longitude = position.longitude
+
+    one_degree_of_latitude_in_meters = 111_000
+    lat_radius = radius / one_degree_of_latitude_in_meters
+    lon_radius = radius / (
+            one_degree_of_latitude_in_meters
+            * math.cos(math.radians(user_latitude))
+    )
+    lat_min = user_latitude - lat_radius
+    lat_max = user_latitude + lat_radius
+    lon_min = user_longitude - lon_radius
+    lon_max = user_longitude + lon_radius
+
+    nearby_items = CollectibleItem.objects.filter(
+        latitude__range=(lat_min, lat_max),
+        longitude__range=(lon_min, lon_max),
+    )
+    user_position = (user_latitude, user_longitude)
+
+    for item in nearby_items:
+        item_position = (item.latitude, item.longitude)
+        if geodesic(user_position, item_position).meters < radius:
+            user.items.add(item)

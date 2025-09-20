45436152e9d5fb3795f7b0c9d8d5a612158f80fe
Alexander Aravin <sander-raven@yandex.ru>
2025-09-20 18:14:16 +0300

    feat: override `create` in `PositionViewSet` to add collectible items

--- app_run/views/position.py
+++ app_run/views/position.py
@@ -1,8 +1,11 @@
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import status
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from app_run.models import Position
 from app_run.serializers import PositionSerializer
+from app_run.helpers import assign_nearby_items_to_user
 
 __all__ = [
     'PositionViewSet',
@@ -14,3 +17,11 @@ class PositionViewSet(ModelViewSet):
     serializer_class = PositionSerializer
     filter_backends = (DjangoFilterBackend,)
     filterset_fields = ('run',)
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        position = serializer.save()
+        assign_nearby_items_to_user(position)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)

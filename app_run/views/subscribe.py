e14cd3bd2f65d5228680debeb345fb0b9385bcf5
Alexander Aravin <sander-raven@yandex.ru>
2025-09-24 21:21:06 +0300

    fix: if coach is not coach, then return 400

--- app_run/views/subscribe.py
+++ app_run/views/subscribe.py
@@ -2,6 +2,7 @@ from django.contrib.auth import get_user_model
 from django.db import IntegrityError
 from rest_framework import status
 from rest_framework.decorators import api_view
+from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
 
 from app_run.models import Subscribe
@@ -16,9 +17,9 @@ __all__ = [
 @api_view(['POST'])
 def subscribe_to_coach(request, coach_id: int) -> Response:
     """Subscribe athlete to coach"""
-    coach = User.objects.filter(pk=coach_id, is_staff=True).first()
-    if coach is None:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+    coach = get_object_or_404(User, pk=coach_id)
+    if not coach.is_staff:
+        return Response(status=status.HTTP_400_BAD_REQUEST)
     athlete_id = request.data.get('athlete')
     athlete = User.objects.filter(pk=athlete_id, is_staff=False).first()
     if athlete is None:

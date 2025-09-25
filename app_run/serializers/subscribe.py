ef4f2fac5e1c6a26e8ee1fb046485a6380f60d31
Alexander Aravin <sander-raven@yandex.ru>
2025-09-25 20:17:56 +0300

    fix: modify `validate_rating`

--- app_run/serializers/subscribe.py
+++ app_run/serializers/subscribe.py
@@ -14,7 +14,7 @@ class SubscribeSerializer(serializers.ModelSerializer):
         )
 
     def validate_rating(self, value):
-        if not (1 <= value <= 5):
+        if type(value) != int or not (1 <= value <= 5):
             raise serializers.ValidationError(
                 'Rating must be between 1 and 5.'
             )

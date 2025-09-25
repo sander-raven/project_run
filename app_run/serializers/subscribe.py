2b1612b357cfdda28a4537e32860b311fd6d2c6c
Alexander Aravin <sander-raven@yandex.ru>
2025-09-25 20:24:39 +0300

    chore: add comment to `validate_rating`

--- app_run/serializers/subscribe.py
+++ app_run/serializers/subscribe.py
@@ -14,6 +14,9 @@ class SubscribeSerializer(serializers.ModelSerializer):
         )
 
     def validate_rating(self, value):
+        # Почему-то на проде без проверки типа падала ошибка,
+        # когда rating имел значение None, хотя локально
+        # отличные от int значения отсеивались средствами фреймворка.
         if type(value) != int or not (1 <= value <= 5):
             raise serializers.ValidationError(
                 'Rating must be between 1 and 5.'
